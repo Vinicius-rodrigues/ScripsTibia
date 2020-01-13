fefee25b266a98aaec55ab5d20edda39524d62e2
Vinicius rodrigues <viniciusrodrigues93@yahoo.com.br>
2020-01-13 06:51:05 -0300

    Add files via upload

---
+++ tranningMl.py
@@ -0,0 +1,79 @@
+import pyautogui as auto
+import time
+
+def moveMouse(inicial, final):
+    auto.moveTo(inicial, final)
+
+
+def pressKeyboard(button):
+     auto.press(str(button))
+
+
+contKeybord = 1000
+
+for c in range(contKeybord):
+
+    time.sleep(3)
+    moveMouse(0,500)
+    pressKeyboard('f12')
+    print( "runoouuu")
+    time.sleep(3)
+    pressKeyboard('f11')
+    time.sleep(47)
+    
+    moveMouse(500,0)
+    moveMouse(0,500)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    auto.press('down')
+    time.sleep(10)
+
+    print(c)
+
+
+
+
